5dd75d0fc5948fce2bb4ef4b6e3aac537a2a95fe
Skydes <paul-edouard.sarlin@orange.fr>
2017-04-20 21:15:55 +0800

    Cleaned the messy control directory

--- src/control/path_finder/createCircle.m
+++
@@ -1,7 +0,0 @@
-function c = createCircle( center, radius )
-
-c.center = center;
-c.radius = radius;
-c.type = 'circle';
-
-end
--- src/control/path_finder/groundCollision.m
+++
@@ -1,9 +0,0 @@
-function [ collision ] = groundCollision( robot, th1th2 )
-
-th1 = th1th2(:,1);
-th2 = th1th2(:,2);
-
-collision = (robot.l1*sin(th1) + robot.l2*sin(th1+th2)) < 0;
-
-end
-
